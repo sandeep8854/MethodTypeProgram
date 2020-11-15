d87ca0e8015f451f87ae9bc1e80f5ca3dbcd6634
Sandeep Prajapati <sandeepstructure@gmail.com>
2020-11-15 11:38:52 +0530

    Benefit of method ,called method to calling.

---
+++ MethodTypeProgram/src/sandeep/com/Method_9.java
@@ -0,0 +1,37 @@
+package sandeep.com;
+public class Method_9 
+{
+	static void msg()   //Called Method
+	{
+		System.out.println(" <------Welcome To------> ");
+		System.out.println("<-------Jspider And Qspider----->");
+	}
+	public static void main(String[] args) 
+	{
+		msg();  //Calling Method
+		System.out.println("<-----BTM Branch------>");
+		
+		msg();
+		System.out.println("<-----BTR Branch------->");
+		
+		msg();
+		System.out.println("<-----Basanvangudi----->");
+		
+		msg();
+		System.out.println("<------Kangara Branch----->");	
+	}
+}
+/*
+<------Welcome To------> 
+<-------Jspider And Qspider----->
+<-----BTM Branch------>
+<------Welcome To------> 
+<-------Jspider And Qspider----->
+<-----BTR Branch------->
+<------Welcome To------> 
+<-------Jspider And Qspider----->
+<-----Basanvangudi----->
+<------Welcome To------> 
+<-------Jspider And Qspider----->
+<------Kangara Branch----->
+*/
